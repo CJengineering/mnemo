451cac20bd4f8851a2ce72b3ba0233bdd473b436
Tim Spiridonov <communityjameeltechnology@gmail.com>
2025-07-17 14:27:32 +0100

    done

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -11,7 +11,6 @@ import {
   Users2
 } from 'lucide-react';
 
-
 import {
   Breadcrumb,
   BreadcrumbItem,
@@ -43,16 +42,12 @@ export default function DashboardLayout({
     <Providers>
       <main className="flex min-h-screen w-full flex-col ">
         <DesktopNav />
-        <div className="flex flex-col sm:gap-4 sm:py-4 sm:pl-14">
-          <header className="sticky top-0 z-30 flex h-14 items-center gap-4 border-b bg-background px-4 sm:static sm:h-auto sm:border-0 sm:bg-transparent sm:px-6">
+        <div className="flex flex-col sm:gap-4  sm:pl-14">
+          {/* <header className="sticky top-0 z-30 flex h-14 items-center gap-4 border-b bg-background px-4 sm:static sm:h-auto sm:border-0 sm:bg-transparent sm:px-6">
             <MobileNav />
-       
-            {/* <SearchInput /> */}
-            <User />
-          </header>
-          <main className="grid flex-1 items-start gap-2 p-4 sm:px-6 sm:py-0 md:gap-4 ">
-            {children}
-          </main>
+            <SearchInput /> <User />
+          </header> */}
+          <main className="">{children}</main>
         </div>
         <Analytics />
       </main>
@@ -80,7 +75,7 @@ function DesktopNav() {
           <PanelsTopLeft className="h-5 w-5" />
         </NavItem>
 
-        <NavItem href="/" label="Products">
+        <NavItem href="/collections" label="Products">
           <Package className="h-5 w-5" />
         </NavItem>
 
